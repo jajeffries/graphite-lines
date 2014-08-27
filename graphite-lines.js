e146fb851b55a7f650d3535ee107e23e163f9a9b
James Jeffries <jamesjeffries1@gmail.com>
2014-08-27 08:55:44 +0100

    corrected inverted axis

--- graphite-lines.js
+++ graphite-lines.js
@@ -1,93 +1,92 @@
-(function () {
+(function (window) {
 
-    function Graph(selector, options) {
+    function graphiteline(selector, options) {
         var options = options || {},
             height = options.height || 200,
             width = options.width || 300,
             padding = options.padding || 60,
             grid = options.grid || true,
-            area = options.area || true;
+            area = options.area || true,
+            lineData = options.data;
         
-        var svgContainer = d3.select(selector).append("svg")
-                                          .attr("width", width)
-                                          .attr("height", height);
+        var svg = d3.select(selector)
+                             .append("svg")
+                             .attr("width", width)
+                             .attr("height", height);
 
-        this.data = function (lineData) {
+        var xScale = d3.scale.linear()
+                                .range([0, width+padding])
+                                .domain(d3.extent(lineData[0].datapoints, function(d) { return d[1]; }));
 
-            var lineFunction = d3.svg.line()
-                                   .x(function(d, i) { return xScale(d[1]) + padding; })
-                                   .y(function(d, i) { return yScale(d[0]); })
-                                   .interpolate("linear");
+        var yScale = d3.scale.linear()
+                                .range([height-padding, 0])
+                                .domain(d3.extent(lineData[0].datapoints, function(d) { return d[0]; }));
 
-            var xScale = d3.scale.linear()
-                                    .range([0, width+padding])
-                                    .domain(d3.extent(lineData[0].datapoints, function(d) { return d[1]; }));
+        var xAxis = d3.svg.axis()
+                          .scale(xScale)
+                          .ticks(5)
+                          .tickFormat(function(d) { 
+                              var date = new Date(d); 
+                              return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(); 
+                          })
+                          .orient("bottom");
 
-            var yScale = d3.scale.linear()
-                                    .range([0, height-padding])
-                                    .domain(d3.extent(lineData[0].datapoints, function(d) { return d[0]; }));
+        svg.append("g")
+           .attr("class", "axis")
+           .attr("transform", "translate(" + padding + "," + (height-padding) + ")")
+           .call(xAxis);
 
-            var xAxis = d3.svg.axis()
-                                .scale(xScale)
-                                .ticks(5)
-                                .tickFormat(function(d) { 
-                                    var date = new Date(d); 
-                                    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(); 
-                                })
-                                .orient("bottom");
+        var yAxis = d3.svg.axis()
+                          .scale(yScale)
+                          .orient("left")
+                          .ticks(10);
 
-            var xAxisGroup = svgContainer.append("g")
-                                         .attr("class", "axis")
-                                         .attr("transform", "translate(" + padding + "," + (height-padding) + ")")
-                                         .call(xAxis);
+        svg.append("g")
+           .attr("class", "axis")
+           .attr("transform", "translate(" + padding + ",0)")
+           .call(yAxis);
 
-            var yAxis = d3.svg.axis()
-                              .scale(yScale)
-                              .orient("left")
-                              .ticks(10);
+        if(grid) {
+            svg.append("g")         
+                    .attr("class", "grid")
+                    .attr("transform", "translate(0," + (height-padding) + ")")
+                    .call(xAxis
+                            .tickSize(-height, 0, 0)
+                            .tickFormat("")
+                    );
 
-            var xAxisGroup = svgContainer.append("g")
-                                         .attr("class", "axis")
-                                         .attr("transform", "translate(" + padding + ",0)")
-                                         .call(yAxis);
+            svg.append("g")         
+                    .attr("class", "grid")
+                    .attr("transform", "translate(" + padding + ",0)")
+                    .call(yAxis
+                            .tickSize(-width, 0, 0)
+                            .tickFormat("")
+                    );
+        }
 
-            if(grid) {
-                svgContainer.append("g")         
-                        .attr("class", "grid")
-                        .attr("transform", "translate(0," + (height-padding) + ")")
-                        .call(xAxis
-                                .tickSize(-height, 0, 0)
-                                .tickFormat("")
-                        );
+        if(area) {
+            var area = d3.svg.area()
+                            .x(function(d) { return xScale(d[1]); })
+                            .y0(height-padding)
+                            .y1(function(d) { return yScale(d[0]); });
 
-                svgContainer.append("g")         
-                        .attr("class", "grid")
-                        .attr("transform", "translate(" + padding + ",0)")
-                        .call(yAxis
-                                .tickSize(-width, 0, 0)
-                                .tickFormat("")
-                        );
-            }
+            svg.append("path")
+                    .datum(lineData[0].datapoints)
+                    .attr("class", "area")
+                    .attr("transform", "translate(" + padding + ",0)")
+                    .attr("d", area);
+        }
 
-            if(area) {
-                var area = d3.svg.area()
-                                .x(function(d) { return xScale(d[1]); })
-                                .y0(height-padding)
-                                .y1(function(d) { return yScale(d[0]); });
+        var lineFunction = d3.svg.line()
+                               .x(function(d, i) { return xScale(d[1]) + padding; })
+                               .y(function(d, i) { return yScale(d[0]); })
+                               .interpolate("linear");
 
-                svgContainer.append("path")
-                        .datum(lineData[0].datapoints)
-                        .attr("class", "area")
-                        .attr("transform", "translate(" + padding + ",0)")
-                        .attr("d", area);
-            }
-
-            var lineGraph = svgContainer.append("path")
-                                      .attr("d", lineFunction(lineData[0].datapoints))
-                                      .attr("stroke", "blue")
-                                      .attr("stroke-width", 2)
-                                      .attr("fill", "none");
-        };
+        svg.append("path")
+           .attr("d", lineFunction(lineData[0].datapoints))
+           .attr("stroke", "blue")
+           .attr("stroke-width", 2)
+           .attr("fill", "none");
     }
-    window.Graph = Graph;
-})();
+    window.graphiteline = graphiteline;
+})(window);
