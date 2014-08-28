449900d88ad4e10cd3683c7d75375991169c2007
James Jeffries <jamesjeffries1@gmail.com>
2014-08-28 09:54:40 +0100

    hover labels

--- graphite-lines.js
+++ graphite-lines.js
@@ -1,4 +1,9 @@
 (function (window) {
+    
+    function epochDateFormat(d) {
+      var date = new Date(d); 
+      return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    }
 
     function graphiteline(selector, options) {
         var options = options || {},
@@ -9,6 +14,7 @@
             area = options.area || true,
             lineData = options.data || [],
             dataLabels = options.dataLabels || true,
+            mouseoverLabels = options.mouseoverLabels || true,
             allLineData = [].concat.apply([], options.data.map(function (d) {
               return d.datapoints;
             }));
@@ -26,10 +32,6 @@
                                 .range([height-padding, 0])
                                 .domain(d3.extent(allLineData, function(d) { return d[0]; }));
 
-        function epochDateFormat(d) {
-          var date = new Date(d); 
-          return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-        }
 
         var xAxis = d3.svg.axis()
                           .scale(xScale)
@@ -70,104 +72,121 @@
                     );
         }
 
-        if(area) {
-            var area = d3.svg.area()
-                            .x(function(d) { return xScale(d[1]); })
-                            .y0(height-padding)
-                            .y1(function(d) { return yScale(d[0]); });
+        drawSeries(svg, lineData, area, padding, height, mouseoverLabels, dataLabels, xScale, yScale);
+    }
 
-            svg.append("path")
-                    .datum(lineData[0].datapoints)
-                    .attr("class", "area")
-                    .attr("transform", "translate(" + padding + ",0)")
-                    .attr("d", area);
-        }
 
-        var lineFunction = d3.svg.line()
-                               .x(function(d, i) { return xScale(d[1]) + padding; })
-                               .y(function(d, i) { return yScale(d[0]); })
-                               .interpolate("linear");
+    function drawSeries(svg, lineData, showArea, padding, height, mouseoverLabels, showDataLabels, xScale, yScale) {
+      for(var i=0; i<lineData.length; i++) {
 
-        
+          drawArea(showArea, svg, lineData[i].datapoints, padding, height, xScale, yScale);
 
-        for(var i=0; i<lineData.length; i++) {
+          var path = drawPath(svg, lineData[i].datapoints, padding, xScale, yScale);
 
-          var path = svg.append("path")
-             .attr("d", lineFunction(lineData[i].datapoints))
-             .attr("stroke", "blue")
-             .attr("stroke-width", 3)
-             .attr("fill", "none");
-             // .classed(lineData[i].target.replace('.','-').toLowerCase());
-
-          if(dataLabels) {
+          if(mouseoverLabels) {
+            var circle = svg
+                          .append("circle")
+                          .attr("r", "10px")
+                          .style("stroke-width", "2px")
+                          .style("stroke", "darkblue")
+                          .style("fill", "lightsteelblue")
+                          .style("opacity", "0.7")
+                          .on('mouseleave', function () {
+                            circle.style("display", "none");
+                          });
             path
-             .on('mouseover', function () {})
-             .on('mousedown', function() {
+              .on('mouseenter', function () {
                 var rangeX = d3.event.x,
                     rangeY = d3.event.y,
                     epoch = xScale.invert(rangeX),
                     reading = yScale.invert(rangeY);
-                svg.select("circle.datapoint").remove();
-                svg
-                  .append("circle")
-                  .attr("cx", (rangeX - 10) + "px")
-                  .attr("cy", (rangeY - 10) + "px")
-                  .attr("r", "10px")
-                  .style("stroke-width", "2px")
-                  .style("stroke", "darkblue")
-                  .style("fill", "lightsteelblue")
-                  .style("opacity", "0.7")
-                  .classed("datapoint", true);
+                circle
+                    .style("display", "block")
+                    .attr("cx", rangeX - 10)
+                    .attr("cy", rangeY - 10);
+                addDataLabels(svg, circle, showDataLabels, xScale, yScale);
+              });
 
-                svg.select("rect.datapoint-label").remove();
-                var label = svg
-                            .append("rect")
-                            .attr("x", (rangeX + 10) + "px")
-                            .attr("y", (rangeY - 20) + "px")
-                            .attr("width", "80px")
-                            .attr("height", "20px")
-                            .style("stroke-width", "2px")
-                            .style("stroke", "darkblue")
-                            .style("fill", "lightsteelblue")
-                            .style("opacity", "0.7")
-                            .classed("datapoint-label", true);
-                
-                svg.select("text.datapoint-label-text").remove();
-                label
-                  .append("text")
-                  .attr("x", rangeX + "px")
-                  .attr("y", rangeY + "px")
-                  .attr("dy", "0.35em")
-                  .text(function () {
-                    return Math.floor(reading) + " - " + epochDateFormat(Math.floor(epoch));
-                  });
-             });
+            
           }
+
+          addDataLabels(svg, path, showDataLabels, xScale, yScale);
         }
+    }
 
-        // var vertical = svg
-        //                   .append("div")
-        //                   .style("position", "absolute")
-        //                   .style("width", "1px")
-        //                   .style("height", (height-padding) + "px")
-        //                   .style("top", padding +"px")
-        //                   .style("bottom", "30px")
-        //                   .style("left", "0px")
-        //                   .style("background", "black");
+    function addDataLabels(svg, object, showDataLabels, xScale, yScale) {
+      if(showDataLabels) {
+        object
+         .on('mousedown', function() {
+            var rangeX = d3.event.x,
+                rangeY = d3.event.y,
+                epoch = xScale.invert(rangeX),
+                reading = yScale.invert(rangeY);
+            svg.select("circle.datapoint").remove();
+            svg
+              .append("circle")
+              .attr("cx", (rangeX - 10) + "px")
+              .attr("cy", (rangeY - 10) + "px")
+              .attr("r", "10px")
+              .style("stroke-width", "2px")
+              .style("stroke", "darkblue")
+              .style("fill", "lightsteelblue")
+              .style("opacity", "0.7")
+              .classed("datapoint", true);
 
-        // svg
-        //   .on("mousemove", function(){ 
-        //     var mousex = d3.mouse(this);
-        //     mousex = mousex[0] + 5;
-        //     console.log("mousemove", mousex);
-        //     vertical.style("left", mousex + "px" );
-        //   })
-        //   .on("mouseover", function(){  
-        //     var mousex = d3.mouse(this);
-        //     mousex = mousex[0] + 5;
-        //     console.log("mouseover", mousex) ;
-        //     vertical.style("left", mousex + "px");
-        //   });
+            svg.select("rect.datapoint-label").remove();
+            var label = svg
+                        .append("rect")
+                        .attr("x", (rangeX + 10) + "px")
+                        .attr("y", (rangeY - 20) + "px")
+                        .attr("width", "80px")
+                        .attr("height", "20px")
+                        .style("stroke-width", "2px")
+                        .style("stroke", "darkblue")
+                        .style("fill", "lightsteelblue")
+                        .style("opacity", "0.7")
+                        .classed("datapoint-label", true);
+            
+            svg.select("text.datapoint-label-text").remove();
+            label
+              .append("text")
+              .attr("x", rangeX + "px")
+              .attr("y", rangeY + "px")
+              .attr("dy", "0.35em")
+              .text(function () {
+                return Math.floor(reading) + " - " + epochDateFormat(Math.floor(epoch));
+              });
+         });
+      }
+    }
+
+    function drawArea(showArea, svg, datapoints, padding, height, xScale, yScale) {
+      if(showArea) {
+        var area = d3.svg.area()
+                        .x(function(d) { return xScale(d[1]); })
+                        .y0(height-padding)
+                        .y1(function(d) { return yScale(d[0]); });
+
+        svg.append("path")
+                .datum(datapoints)
+                .attr("class", "area")
+                .attr("transform", "translate(" + padding + ",0)")
+                .attr("d", area);
+      }
+    }
+
+    function drawPath(svg, datapoints, padding, xScale, yScale) {
+      var lineFunction = d3.svg.line()
+                               .x(function(d, i) { return xScale(d[1]) + padding; })
+                               .y(function(d, i) { return yScale(d[0]); })
+                               .interpolate("linear");
+
+      return svg.append("path")
+                .attr("d", lineFunction(datapoints)) 
+                .attr("stroke", "blue")
+                .attr("stroke-width", 3)
+                .attr("fill", "none");
+                // .classed(lineData[i].target.replace('.','-').toLowerCase());
     }
     window.graphiteline = graphiteline;
 })(window);
